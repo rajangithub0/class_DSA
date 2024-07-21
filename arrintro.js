1fe8ddfa826cb4cc14f6ce073eaed99f2e2af24b
Rajan <122583123+rajangithub0@users.noreply.github.com>
2024-07-21 22:40:19 +0530

    dsa with array

---
+++ arrintro.js
@@ -0,0 +1,12 @@
+var arr = [10, 20, 30, 40, 50, 60];
+var fruits = ['apple', 'orange', 'banana', 'lichi'];
+fruits.push('mango');
+console.log(fruits[2]);
+console.log(fruits[fruits.length - 1]);
+fruits.pop();
+fruits.shift();
+console.log(fruits[0]);
+fruits.unshift('graps');
+console.log(fruits[0]);
+arr[3] = 100;
+console.log(arr);
